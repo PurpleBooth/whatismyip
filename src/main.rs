2b88f9c8b8428f8b201c377836c927ed875c978b
Billie Thompson <billie@billiecodes.com>
2021-06-17 19:51:20 +0200

    fix: Use IPv4 and IPv6 rather than 4 then 6

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,19 @@
 use anyhow::anyhow;
 use anyhow::Result;
 use std::net::IpAddr;
-use trust_dns_resolver::config::{NameServerConfigGroup, ResolverConfig, ResolverOpts};
+use trust_dns_resolver::config::{
+    LookupIpStrategy, NameServerConfigGroup, ResolverConfig, ResolverOpts,
+};
 use trust_dns_resolver::Resolver;
+
+use std::time::Duration;
 
 mod cli;
 
 fn main() -> Result<()> {
     cli::app().get_matches();
 
-    let ns_ip = resolver_ip()?;
+    let ns_ip = resolver_ip("ns1.google.com")?;
     let resolver = resolver(ns_ip)?;
     let user_ips = user_ips(&resolver)?;
     println!("{}", user_ips);
@@ -33,13 +37,34 @@ fn resolver(ip: IpAddr) -> Result<Resolver> {
             vec![],
             NameServerConfigGroup::from_ips_clear(&[ip], 53, true),
         ),
-        ResolverOpts::default(),
+        resolver_opts(),
     )?)
 }
 
-fn resolver_ip() -> Result<IpAddr> {
+fn resolver_opts() -> ResolverOpts {
+    ResolverOpts {
+        ndots: 1,
+        timeout: Duration::from_secs(5),
+        attempts: 2,
+        rotate: false,
+        check_names: true,
+        edns0: false,
+        validate: false,
+        ip_strategy: LookupIpStrategy::Ipv4AndIpv6,
+        cache_size: 32,
+        use_hosts_file: true,
+        positive_min_ttl: None,
+        negative_min_ttl: None,
+        positive_max_ttl: None,
+        negative_max_ttl: None,
+        num_concurrent_reqs: 2,
+        preserve_intermediates: false,
+    }
+}
+
+fn resolver_ip(ns_host: &str) -> Result<IpAddr> {
     Resolver::default()?
-        .lookup_ip("ns1.google.com")?
+        .lookup_ip(ns_host)?
         .iter()
         .next()
         .ok_or_else(|| anyhow!("Nameserver ip not found"))
