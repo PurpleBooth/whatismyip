c6ecc66a11161dbb645d7be88624602383e3d397
Billie Thompson <billie@billiecodes.com>
2021-06-19 09:04:01 +0200

    feat: Always list both ips
    This will always list both ipv6 ips and ipv4 ips if it's possible

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,80 @@
-use anyhow::anyhow;
-use anyhow::Result;
 use std::net::IpAddr;
+use std::time::Duration;
+
+use anyhow::anyhow;
+use anyhow::bail;
+use anyhow::Result;
 use trust_dns_resolver::config::{
     LookupIpStrategy, NameServerConfigGroup, ResolverConfig, ResolverOpts,
 };
-use trust_dns_resolver::Resolver;
-
-use std::time::Duration;
+use trust_dns_resolver::{AsyncResolver, TokioAsyncResolver};
 
 mod cli;
 
-fn main() -> Result<()> {
+#[tokio::main]
+async fn main() -> Result<()> {
     cli::app().get_matches();
 
-    let ns_ip = resolver_ip("ns1.google.com")?;
-    let resolver = resolver(ns_ip)?;
-    let user_ips = user_ips(&resolver)?;
-    println!("{}", user_ips);
+    match tokio::join!(
+        find_ip(LookupIpStrategy::Ipv4Only),
+        find_ip(LookupIpStrategy::Ipv6Only)
+    ) {
+        (Err(ipv4_error), Err(ipv6_error)) => {
+            bail!(
+                "IPv6 and IPv4 both failed: IPv6 \"{}\" and IPv4 \"{}\"",
+                ipv6_error,
+                ipv4_error
+            );
+        }
+        (Ok(ipv4), Err(_)) => {
+            println!("{}", ipv4);
+        }
+        (Err(_), Ok(ipv6)) => {
+            println!("{}", ipv6);
+        }
+        (Ok(ipv4), Ok(ipv6)) => {
+            println!("{}", ipv4);
+            println!("{}", ipv6);
+        }
+    }
 
     Ok(())
 }
 
-fn user_ips(resolver: &Resolver) -> Result<String> {
+async fn find_ip(strategy: LookupIpStrategy) -> Result<String> {
+    let ns_ip = tokio::select! {
+        ns_ip = async { resolver_ip("ns1.google.com", strategy).await } => ns_ip,
+        ns_ip = async { resolver_ip("ns2.google.com", strategy).await } => ns_ip,
+        ns_ip = async { resolver_ip("ns3.google.com", strategy).await } => ns_ip,
+        ns_ip = async { resolver_ip("ns4.google.com", strategy).await } => ns_ip,
+    }?;
+
+    let dns_resolver = resolver(ns_ip, LookupIpStrategy::Ipv4Only)?;
+    user_ips(&dns_resolver).await
+}
+
+async fn user_ips(resolver: &TokioAsyncResolver) -> Result<String> {
     Ok(resolver
-        .txt_lookup("o-o.myaddr.l.google.com")?
+        .txt_lookup("o-o.myaddr.l.google.com")
+        .await?
         .iter()
         .map(std::string::ToString::to_string)
         .collect::<Vec<_>>()
         .join("\n"))
 }
 
-fn resolver(ip: IpAddr) -> Result<Resolver> {
-    Ok(Resolver::new(
+fn resolver(ip: IpAddr, ip_strategy: LookupIpStrategy) -> Result<TokioAsyncResolver> {
+    Ok(AsyncResolver::tokio(
         ResolverConfig::from_parts(
             None,
             vec![],
             NameServerConfigGroup::from_ips_clear(&[ip], 53, true),
         ),
-        resolver_opts(),
+        resolver_opts(ip_strategy),
     )?)
 }
 
-fn resolver_opts() -> ResolverOpts {
+fn resolver_opts(ip_strategy: LookupIpStrategy) -> ResolverOpts {
     ResolverOpts {
         ndots: 1,
         timeout: Duration::from_secs(5),
@@ -50,7 +83,7 @@ fn resolver_opts() -> ResolverOpts {
         check_names: true,
         edns0: false,
         validate: false,
-        ip_strategy: LookupIpStrategy::Ipv4AndIpv6,
+        ip_strategy,
         cache_size: 32,
         use_hosts_file: true,
         positive_min_ttl: None,
@@ -62,9 +95,10 @@ fn resolver_opts() -> ResolverOpts {
     }
 }
 
-fn resolver_ip(ns_host: &str) -> Result<IpAddr> {
-    Resolver::default()?
-        .lookup_ip(ns_host)?
+async fn resolver_ip(ns_host: &str, ip_strategy: LookupIpStrategy) -> Result<IpAddr> {
+    AsyncResolver::tokio(ResolverConfig::default(), resolver_opts(ip_strategy))?
+        .lookup_ip(ns_host)
+        .await?
         .iter()
         .next()
         .ok_or_else(|| anyhow!("Nameserver ip not found"))
