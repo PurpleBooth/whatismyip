76429a765baa1baafefb6f5223ac37f82f182d70
Billie Thompson <billie@purplebooth.co.uk>
2025-05-12 08:54:04 +0200

    refactor: simplify CLI argument attribute definitions

--- src/cli.rs
+++ src/cli.rs
@@ -8,38 +8,18 @@ use clap::Parser;
 #[clap(author, version, about, long_about = None)]
 pub struct Args {
     /// Only print IP addresses local to this machine
-    #[clap(
-        short = 'l',
-        long = "only-local",
-        conflicts_with = "only_wan",
-        hide_env_values(true)
-    )]
+    #[clap(short = 'l', long = "only-local", conflicts_with = "only_wan")]
     pub only_local: bool,
     /// Only print IP addresses as seen by a remote service
-    #[clap(
-        short = 'w',
-        long = "only-wan",
-        conflicts_with = "only_local",
-        hide_env_values(true)
-    )]
+    #[clap(short = 'w', long = "only-wan", conflicts_with = "only_local")]
     pub only_wan: bool,
     /// Only print IPv4 addresses
-    #[clap(
-        short = '4',
-        long = "only-4",
-        conflicts_with = "only_6",
-        hide_env_values(true)
-    )]
+    #[clap(short = '4', long = "only-4", conflicts_with = "only_6")]
     pub only_4: bool,
     /// Only print IPv6 addresses
-    #[clap(
-        short = '6',
-        long = "only-6",
-        conflicts_with = "only_4",
-        hide_env_values(true)
-    )]
+    #[clap(short = '6', long = "only-6", conflicts_with = "only_4")]
     pub only_6: bool,
     /// Print the reverse DNS entries for the IP addresses
-    #[clap(short = 'r', long = "reverse", hide_env_values(true))]
+    #[clap(short = 'r', long = "reverse")]
     pub reverse: bool,
 }
